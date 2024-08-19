a5eea798df937879b7654542bc390d5f9961d488
Oliver Jenkins <cyberforge1@gmail.com>
2024-08-19 11:06:50 +1000

    updated db schema

--- schema.sql
+++ schema.sql
@@ -1,4 +1,3 @@
-
 CREATE TABLE ufo_sightings (
     id SERIAL PRIMARY KEY,
     datetime TIMESTAMP,
@@ -14,8 +13,33 @@ CREATE TABLE ufo_sightings (
     longitude FLOAT
 );
 
+CREATE TABLE ufo_sightings_australia (
+    id SERIAL PRIMARY KEY,
+    datetime TIMESTAMP,
+    city VARCHAR(100),
+    state VARCHAR(50),
+    country VARCHAR(50),
+    shape VARCHAR(50),
+    duration_seconds FLOAT,
+    duration_hours_min VARCHAR(50),
+    comments TEXT,
+    date_posted DATE,
+    latitude FLOAT,
+    longitude FLOAT,
+    original_id INT,
+    FOREIGN KEY (original_id) REFERENCES ufo_sightings(id)
+);
+
+INSERT INTO ufo_sightings_australia (datetime, city, state, country, shape, duration_seconds, duration_hours_min, comments, date_posted, latitude, longitude, original_id)
+SELECT datetime, city, state, country, shape, duration_seconds, duration_hours_min, comments, date_posted, latitude, longitude, id
+FROM ufo_sightings
+WHERE country = 'AU'
+   OR (latitude BETWEEN -44.0 AND -10.0 AND longitude BETWEEN 112.0 AND 154.0);
+
+
 DROP TABLE IF EXISTS ufo_sightings;
+DROP TABLE IF EXISTS ufo_sightings_australia;
 
 SELECT * FROM ufo_sightings;
 
- 
+SELECT * FROM ufo_sightings_australia;
